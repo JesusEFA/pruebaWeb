e53dd0b41b8aaa6ff6b17e685bedfbfec8b5d47e
JesusEFA <94001781+JesusEFA@users.noreply.github.com>
2025-04-01 13:15:10 +0200

    Update WebFinalEsperanzaApplication.java

--- src/main/java/com/hermandad_virgenesperanza/web/WebFinalEsperanzaApplication.java
+++ src/main/java/com/hermandad_virgenesperanza/web/WebFinalEsperanzaApplication.java
@@ -3,6 +3,7 @@ package com.hermandad_virgenesperanza.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
 public class WebFinalEsperanzaApplication {
 
